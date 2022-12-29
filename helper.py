c2aa9179b30b6c8ac25775dd2798ba3873dbb118
salisale <salisaletheia@gmail.com>
2022-12-30 00:53:32 +0700

    corrected prime fn

--- helper.py
+++ helper.py
@@ -1,10 +1,8 @@
 
-import math
-
 def isPrime(x):
 	if x <= 5: return True if x in (2,3,5) else False
 	if x%2==0 or x%3==0 or x%5==0 : return False
-	for i in range(2, math.ceil(x/2)):
+	for i in range(2, int(x**0.5)+1): # 2-> sqrt(x)
 		if x%i==0:
 			return False
 	return True
